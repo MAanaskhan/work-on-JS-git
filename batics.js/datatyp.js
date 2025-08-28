49d586f3444a29eed80ab9c78a82bbb9b672353c
MAanaskhan <onlineforyou33@gmail.com>
2025-08-28 11:31:28 +0000

    datatyp

---
+++ batics.js/datatyp.js
@@ -0,0 +1,19 @@
+console.log("check this")
+
+"use strict";//--> Treat all js code in a newer version og js
+// alert("this hower"); we are using node.js not a brower in a browser it will run 
+// -->PRIMITIVE DATATYPES 
+let name ="anas" //string
+let age =23 //int
+let result = true // boolean false
+// null  -->stand alone value -->when you have no idea of the data to assign
+// undefined -->is when a value is nt assgned to the varibale 
+
+// null (datatype and also a stand alone value)
+
+// DERIVED DATA TYPES  
+// object 
+// null is also an object 
+console.log(typeof undefined) // this typeof  wil show you what is the type of this value
+
+
